ec5882148eac5e5bcdb791aef7448951db3ad0d1
Paul Kassar <paulkassar@gmail.com>
2017-11-09 19:51:40 +0000

    comitting since computer is dying

--- src/App.js
+++ src/App.js
@@ -9,32 +9,45 @@ const CustomerRow = ({customer}) => (
     <td>{customer.profession}</td>
     <td>{customer.has_children ? "Y" : "N"}</td>
     <td>
-      {customer.dominant_traits[0].level} {customer.dominant_traits[0].primary_trait},
-      {customer.dominant_traits[1].level} {customer.dominant_traits[1].secondary_trait}
-      <PieChart
-      slices={[
-        {
-          color: '#f00',
-          value: customer.metrics.agreeableness,
-        },
-        {
-          color: '#ff8a00',
-          value: customer.metrics.extroversion,
-        },
-        {
-          color: '#0094ff',
-          value: customer.metrics.conscientiousness,
-        },
-        {
-          color: '#0500ff',
-          value: customer.metrics.openness,
-        },
-        {
-          color: '#00b92e',
-          value: customer.metrics.neuroticism,
-        },
-      ]}
-    />
+      <div className="traits">
+        <ul className="trait-details">
+          <li>
+          <span className="key" />
+          {customer.dominant_traits[0].level} {customer.dominant_traits[0].primary_trait},
+          </li>
+          <li>
+          <span className="key" />
+          {customer.dominant_traits[1].level} {customer.dominant_traits[1].secondary_trait}
+          </li>
+        </ul>
+        <div className="pie">
+          <PieChart
+          slices={[
+            {
+              color: '#f00',
+              value: customer.metrics.agreeableness,
+            },
+            {
+              color: '#ff8a00',
+              value: customer.metrics.extroversion,
+            },
+            {
+              color: '#0094ff',
+              value: customer.metrics.conscientiousness,
+            },
+            {
+              color: '#0500ff',
+              value: customer.metrics.openness,
+            },
+            {
+              color: '#00b92e',
+              value: customer.metrics.neuroticism,
+            },
+          ]}
+        />
+        </div>
+      </div>
+
     </td>
   </tr>
 );
@@ -42,17 +55,18 @@ const CustomerRow = ({customer}) => (
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div id="app">
         <header>
           <h1>Walnut</h1>
+          <h2> Customer Information System </h2>
         </header>
-        <table>
+        <table className="client-data" cellspacing="0">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Profession</th>
+              <th>Age / Sex</th>
+              <th>Name / Profession</th>
               <th>Children</th>
-              <th>Traits</th>
+              <th>Dominant Traits</th>
 
             </tr>
           </thead>
